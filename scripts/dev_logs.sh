ddf19b7876d2a814b5987c40efa0950a7fa72e2f
Tamirat Dejene <tamirat.dejenie@gmail.com>
2026-03-18 20:21:18 +0300

    feat: add script to fetch and follow logs for multiple services in development

---
+++ scripts/dev_logs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+COMPOSE_FILE="/home/tamirat-dejene/Documents/coursework/final_project/veritas/docker-compose.yml"
+
+docker compose -f "$COMPOSE_FILE" logs -f --tail=100 -t \
+  api-gateway auth-service candidate-service enterprise-service exam-service \
+  face-verification-service grading-service grading-service payment-service proctoring-service
