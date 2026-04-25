49a652a0c2d74c2b8053a2b4c452ef65c080095c
Tamirat Dejene <tamirat.dejenie@gmail.com>
2026-04-25 23:03:22 +0300

    feat(shared): add new messaging topics for enterprise and user lifecycles

--- shared/pkg/messaging/topics/topics.go
+++ shared/pkg/messaging/topics/topics.go
@@ -10,8 +10,18 @@ const (
 // Enterprise-Service topics
 const (
 	EnterpriseCreated                = "enterprise.enterprise.created"
+	EnterpriseApproved               = "enterprise.enterprise.approved"
+	EnterpriseSuspended              = "enterprise.enterprise.suspended"
+	EnterpriseDeleted                = "enterprise.enterprise.deleted"
+	EnterpriseHardDeleted            = "enterprise.enterprise.hard_deleted"
+	EnterpriseReactivated            = "enterprise.enterprise.reactivated"
+	EnterpriseRestored               = "enterprise.enterprise.restored"
 	EnterpriseStaffCreated           = "enterprise.staff.created"
 	EnterprisePasswordResetRequested = "enterprise.password.reset.requested"
+	UserDeactivated                  = "enterprise.user.deactivated"
+	UserActivated                    = "enterprise.user.activated"
+	UserPasswordChanged              = "enterprise.user.password.changed"
+	UserPasswordResetAdmin           = "enterprise.user.password.reset.admin"
 )
 
 // Payment-Service topics
