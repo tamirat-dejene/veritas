461d7f989ad342cb8bc6d64b92a3b8e0eeea5972
Tamirat Dejene <tamirat.dejenie@gmail.com>
2026-04-25 23:03:52 +0300

    feat(notification): add HTML email templates and embeddings

--- services/notification-service/internal/template/embed.go
+++ services/notification-service/internal/template/embed.go
@@ -9,3 +9,36 @@ var WelcomeStaffEmail string
 
 //go:embed password_reset_email.html
 var PasswordResetEmail string
+
+//go:embed enterprise_registered_email.html
+var EnterpriseRegisteredEmail string
+
+//go:embed enterprise_approved_email.html
+var EnterpriseApprovedEmail string
+
+//go:embed enterprise_suspended_email.html
+var EnterpriseSuspendedEmail string
+
+//go:embed enterprise_deleted_email.html
+var EnterpriseDeletedEmail string
+
+//go:embed enterprise_hard_deleted_email.html
+var EnterpriseHardDeletedEmail string
+
+//go:embed enterprise_reactivated_email.html
+var EnterpriseReactivatedEmail string
+
+//go:embed enterprise_restored_email.html
+var EnterpriseRestoredEmail string
+
+//go:embed user_deactivated_email.html
+var UserDeactivatedEmail string
+
+//go:embed user_activated_email.html
+var UserActivatedEmail string
+
+//go:embed user_password_changed_email.html
+var UserPasswordChangedEmail string
+
+//go:embed user_password_reset_admin_email.html
+var UserPasswordResetAdminEmail string
