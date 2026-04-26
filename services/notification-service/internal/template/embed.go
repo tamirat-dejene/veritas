a93c84a589028b516229c668099f97c191c5ce8e
Tamirat Dejene <tamirat.dejenie@gmail.com>
2026-04-27 00:55:15 +0300

    feat(notification): add premium html templates for exam lifecycle

--- services/notification-service/internal/template/embed.go
+++ services/notification-service/internal/template/embed.go
@@ -42,3 +42,24 @@ var UserPasswordChangedEmail string
 
 //go:embed user_password_reset_admin_email.html
 var UserPasswordResetAdminEmail string
+
+//go:embed exam_created_admin.html
+var ExamCreatedAdminEmail string
+
+//go:embed exam_scheduled_admin.html
+var ExamScheduledAdminEmail string
+
+//go:embed exam_scheduled_candidate.html
+var ExamScheduledCandidateEmail string
+
+//go:embed exam_published_admin.html
+var ExamPublishedAdminEmail string
+
+//go:embed exam_published_candidate.html
+var ExamPublishedCandidateEmail string
+
+//go:embed exam_closed_admin.html
+var ExamClosedAdminEmail string
+
+//go:embed exam_closed_candidate.html
+var ExamClosedCandidateEmail string
