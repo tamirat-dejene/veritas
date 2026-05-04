324ab7696a6328ac813707464b1b14dc823a24d5
Tamirat Dejene <tamirat.dejenie@gmail.com>
2026-05-04 20:55:50 +0300

    fix(db): cleanup redundant down section in migration 000002 up file

--- services/payment-service/migrations/000002_add_stripe_price_id_to_plans.up.sql
+++ services/payment-service/migrations/000002_add_stripe_price_id_to_plans.up.sql
@@ -1,5 +1,2 @@
 -- +migrate Up
 ALTER TABLE veritas_subscription_plans ADD COLUMN stripe_price_id VARCHAR(100);
-
--- +migrate Down
-ALTER TABLE veritas_subscription_plans DROP COLUMN stripe_price_id;
